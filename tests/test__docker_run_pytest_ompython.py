7bca3319b8dedafbb40c4dbd195a041e9cb10d15
KangWon LEE <kangwon@gmail.com>
2025-02-26 15:06:03 +0900

    feature : try to compile a .mos script

--- tests/test__docker_run_pytest_ompython.py
+++ tests/test__docker_run_pytest_ompython.py
@@ -53,5 +53,29 @@ def test__docker_run_pytest_omc():
     )
 
 
+def test__docker_run_pytest_omc_compile_export_fmu():
+    # docker run --rm --volume "$HOME:${{ github.workspace }}" --env "HOME=${{ github.workspace }}" --workdir "$PWD" --user $UID openmodelica/openmodelica:v1.24.4-ompython python -m pytest tests/test__check_ompython.py
+    p = subprocess.run(
+        (
+            "docker", "run", "--rm",
+            "--volume", f"{os.environ['HOME']}:{os.environ['GITHUB_WORKSPACE']}",
+            "--env", f"HOME={os.environ['GITHUB_WORKSPACE']}",
+            "--workdir", f"{os.environ['PWD']}",
+            "--user", f"{os.getuid()}",
+            "openmodelica/openmodelica:v1.24.4-ompython",
+            "omc", "exportFMU.mos",
+        ),
+        capture_output=True,
+        text=True,
+    )
+    # could successfully run the command
+    assert p.returncode == 0, (
+        '\n'
+        f"Return code: {p.returncode}\n"
+        f"stdout:\n{p.stdout}\n"
+        f"stderr:\n{p.stderr}"
+    )
+
+
 if "__main__" == __name__:
     pytest.main([__file__])
